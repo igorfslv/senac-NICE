1339377adcc9537a585862217d8a95c55effbbb3
Igor Felipe da Silva <igorf.slv@gmail.com>
2024-09-12 16:45:14 -0300

    feat: add Exceptions-Rest-Produtos

---
+++ api/api/src/main/java/pi/nice/api/errors/RequestExceptionHandler.java
@@ -0,0 +1,16 @@
+package pi.nice.api.errors;
+
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RequestExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratar404(){
+        return ResponseEntity.badRequest().body("Produto não encontrado!");
+    }
+}
