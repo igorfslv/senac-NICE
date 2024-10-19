2cb088f08025587c5a816021683d093efe9252c4
eliana-eml <eml.eliana57@hotmail.com>
2024-10-18 23:40:30 -0300

    feat: botão de log-out

--- front/nice-store/js/pages/header.js
+++ front/nice-store/js/pages/header.js
@@ -9,14 +9,22 @@ function atualizarHeaderLogin() {
         divUsuarioCarrinho.innerHTML = "";
         divUsuarioCarrinho.innerHTML += `
         <p class="msg-bem-vindo-cliente">Bem vindo (a), ${usuarioLogado.nome}!</p>
-        <i class='bx bx-cart-alt btn-carrinho' onclick="window.location.href = '/front/nice-store/pages/carrinho.html'">
-        <span id="badge-carrinho" class="quantidade-itens">0</span>
+        <i class='bx bx-user' title="Meu Cadastro" onclick="window.location.href = '/front/nice-store/pages/atualizacao-cadastro-cliente.html'"></i>
+        <i class='bx bx-cart-alt btn-carrinho' title="Carrinho" onclick="window.location.href = '/front/nice-store/pages/carrinho.html'">
+            <span id="badge-carrinho" class="quantidade-itens">0</span>
         </i>
-        <i class='bx bxs-user'></i>
+        <i class='bx bx-log-out' id="btn-log-out" title="Sair"></i>
         `;
         
         const quantidadeItens = document.getElementById('badge-carrinho');
         quantidadeItens.innerHTML = produtos.length;
+
+        const btnLogOut = document.getElementById('btn-log-out');
+        btnLogOut.addEventListener('click', () => {
+            localStorage.removeItem('usuarioLogado');
+            alert("Você se desconectou da sua conta.")
+            window.location.href = "/front/nice-store/pages/login-cliente.html";
+        });
     }
 
 }
